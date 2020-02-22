c83af1a6d7d689f5696fa8ee95e698cd1582f78c
Greg Roberts <groberts3104@gmail.com>
2020-02-22 16:49:21 -0500

    added function to create workout db

--- routes/api.js
+++ routes/api.js
@@ -1,2 +1,13 @@
 const router = require("express").Router();
-const workout = require("../models/workout.js");
+const Workout = require("../models/workout.js");
+
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+  
