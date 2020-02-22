ab35aec18f4426544748a2333f3014b51fb478ef
Greg Roberts <groberts3104@gmail.com>
2020-02-22 16:59:27 -0500

    added route to get all workouts

--- routes/api.js
+++ routes/api.js
@@ -20,3 +20,17 @@ router.post("/api/workouts/bulk", ({ body }, res) => {
             res.status(400).json(err);
         });
 });
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+      .sort({ date: -1 })
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+  
+  module.exports = router;
+  
