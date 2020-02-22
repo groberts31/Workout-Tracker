73f9a34a34028f2abf21d2f11fbc3cd716699d6a
Greg Roberts <groberts3104@gmail.com>
2020-02-22 17:48:52 -0500

    added new schema info for db

--- models/workout.js
+++ models/workout.js
@@ -1,3 +1,40 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: () => new Date()
+    },
+    exercises: [{
+        type: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        duration: {
+            type: Number,
+            required: true
+        },
+        weight: {
+            type: Number,
+        },
+        reps: {
+            type: Number,
+        },
+        sets: {
+            type: Number,
+        },
+    }],
+},
+    {
+        toJSON: { virtuals: true }
+    });
+    
+  });
