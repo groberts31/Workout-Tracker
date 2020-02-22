1ab68273af8ca6ad76caa38d9acc84d951da2477
Greg Roberts <groberts3104@gmail.com>
2020-02-22 17:20:23 -0500

    added router to stats.html

--- models/html.js
+++ models/html.js
@@ -4,3 +4,7 @@ const path = require("path");
 router.get('/exercise', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/exercise.html'));
 });
+
+router.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
+});
